b097880d68c950a34de12fcade79a82c60ac5ff0
owenrud <owenrudiantoso@gmail.com>
2022-11-23 18:32:23 +0700

    Pertemuan 12 Blazor

--- Blazorwasm/Program.cs
+++ Blazorwasm/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Blazorwasm;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,4 +10,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMudServices();
+
 await builder.Build().RunAsync();
+
+
